0a2f0006c4c7c8b419bd70e7055a74e2e9f78e26
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-12 22:17:50 -0500

    L2-3

--- app/models/calendar.js
+++ app/models/calendar.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name:attr('string'),
-  notes:attr('string',{defaultValue:''}),
-  days:hasMany('day')
+  name:DS.attr('string'),
+  notes:DS.attr('string',{defaultValue:''}),
+  days:DS.hasMany('day')
 });
